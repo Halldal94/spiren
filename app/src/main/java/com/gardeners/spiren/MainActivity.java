41888ed3c2e92ff6ef34ced524ae21e8abaccfa6
Håkon Halldal <hakon.halldal@gmail.com>
2019-02-20 14:59:59 +0100

    AR sett anchors on ground, basic model for plant, progressbars text and buttons added

---
+++ app/src/main/java/com/gardeners/spiren/MainActivity.java
@@ -0,0 +1,164 @@
+package com.gardeners.spiren;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private ArFragment arFragment;
+    private ModelRenderable andyRenderable;
+    //private Session session;
+
+    private Plant plant;
+
+    private TextView height;
+    private ProgressBar health;
+    private ProgressBar water;
+    private ProgressBar fertalizer;
+
+    private Button waterBtn;
+    private Button fertalizeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+        //Setting up AR view
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        //Setting up text and progress bars
+        plant = new Plant();
+        height = (TextView) findViewById(R.id.Height);
+        health = (ProgressBar) findViewById(R.id.Health);
+        water = (ProgressBar) findViewById(R.id.water);
+        fertalizer = (ProgressBar) findViewById(R.id.fertelizer);
+
+        updateInfo();
+
+        //Setting up buttons
+        waterBtn = (Button) findViewById(R.id.waterBtn);
+        fertalizeBtn = (Button) findViewById(R.id.fertalizeBtn);
+
+        waterBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                plant.waterPlant();
+                updateInfo();
+            }
+        });
+
+        fertalizeBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                plant.fertelizPlant();
+                updateInfo();
+            }
+        });
+
+        //AugmentedImageDatabase imageDatabase = new AugmentedImageDatabase(session);
+
+        // When you build a Renderable, Sceneform loads its resources in the background while returning
+        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (andyRenderable == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create the transformable andy and add it to the anchor.
+                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+                    andy.setParent(anchorNode);
+                    andy.setRenderable(andyRenderable);
+                    andy.select();
+                });
+    }
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * <p>Finishes the activity if Sceneform can not run
+     */
+
+    public void onDrawFrame(){
+        Log.i("System is", "drawing frame");
+    }
+
+    public void updateInfo(){
+        height.setText(plant.getLength());
+        health.setProgress(plant.getHealth());
+        water.setProgress(plant.getWater());
+        fertalizer.setProgress(plant.getFertelizer());
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/gardeners/spiren/Plant.java
@@ -0,0 +1,50 @@
+package com.gardeners.spiren;
+
+public class Plant {
+
+    private int length;
+    private int health;
+    private int water;
+    private int fertelizer;
+    private int members;
+
+    public Plant(){
+        fetchData();
+        stupidNoDataInit();
+    }
+
+    private void fetchData(){
+        //TODO: implement
+    }
+    private void stupidNoDataInit(){
+        this.length = 20;
+        this.health = 50;
+        this.water = 30;
+        this.fertelizer = 70;
+        this.members = 5;
+    }
+
+    public String getLength() {
+        return length + " cm";
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getFertelizer() {
+        return fertelizer;
+    }
+
+    public void waterPlant(){
+        
+    }
+
+    public void fertelizPlant(){
+        //TODO: Implement
+    }
+}
